0b3678fb45f7aed702e8daaf58370ddd00e6f1b9
Sergey Poznyak <sergei.pozniak@akvelon.com>
2018-05-11 22:17:32 +0300

    Fixed an MMSI validation error

--- imports/api/vesselLocation/api.js
+++ imports/api/vesselLocation/api.js
@@ -4,7 +4,7 @@ import { HTTP } from 'meteor/http';
 
 Meteor.methods({
   async 'vesselLocation.find'(mmsi) {
-    check(mmsi, String);
+    check(mmsi, Number);
     if (this.isSimulation) {
       return;
     }
