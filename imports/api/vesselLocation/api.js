d685667f6db8e391f89625f719db6b4f6b44a3c2
Sergey Poznyak <sergei.pozniak@akvelon.com>
2018-05-11 16:23:07 +0300

    Improved error handling in vesselLocation

--- imports/api/vesselLocation/api.js
+++ imports/api/vesselLocation/api.js
@@ -3,12 +3,15 @@ import { check } from 'meteor/check';
 import { HTTP } from 'meteor/http';
 
 Meteor.methods({
-  'vesselLocation.find'(mmsi) {
-    if (!this.isSimulation) {
-      let res = null;
+  async 'vesselLocation.find'(mmsi) {
+    if (this.isSimulation) {
+      return;
+    }
+    let res = null;
 
-      try {
-        res = HTTP.get(
+    try {
+      res = await (new Promise((resolve, reject) =>
+        HTTP.get(
           'https://api.aprs.fi/api/get',
           {
             params: {
@@ -17,21 +20,27 @@ Meteor.methods({
               apikey: '109676.BrVSr9i6TpbMbBe',
               format: 'json'
             }
+          },
+          (err, resp) => {
+            if (err) {
+              return reject(err);
+            }
+            resolve(resp);
           }
-        );
+        )
+      ));
 
-        if (res.data.result === 'fail') {
-          throw new new Meteor.Error('vesselLocation.find.api_error', '');
-        }
-      } catch (err) {
-        throw new Meteor.Error('vesselLocation.find.api_error', 'Failed to get location data');
+      if (!res.data || res.data.result === 'fail') {
+        throw new Meteor.Error('vesselLocation.find.api_error', '');
       }
-
-      if (res.data.found === 0) {
-        throw new Meteor.Error('vesselLocation.find.not_found', 'Vessel location is not found');
-      }
-
-      return res.data.entries[0];
+    } catch (err) {
+      throw new Meteor.Error('vesselLocation.find.api_error', 'Failed to get location data');
     }
+
+    if (!res.data || res.data.found === 0) {
+      throw new Meteor.Error('vesselLocation.find.not_found', 'Vessel location is not found');
+    }
+
+    return res.data.entries[0];
   }
 })
